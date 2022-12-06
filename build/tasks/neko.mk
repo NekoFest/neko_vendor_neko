8ada2f97c8d12a19d755102d7646d67dc4ce2754
Vayruz Rafli <frost@nusantararom.org>
2022-12-06 06:52:25 +0000

    neko: Refactor compilation completion
     * based on https://github.com/NusantaraProject-ROM/android_vendor_nusantara/commit/a79347085a275ae614e48ac04207600f41efd896
    
    Co-authored-by: =?UTF-8?q?Andr=C3=A9=20Pinela?= <sheffzor@gmail.com>
    Signed-off-by: Vayruz Rafli <frost@nusantararom.org>
    Change-Id: I334c51d904d6634311b2400556aa0c3abd05fbdc

--- build/tasks/neko.mk
+++ build/tasks/neko.mk
@@ -15,6 +15,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# Build system colors
+#
+# PFX: Prefix "target C++:" in yellow
+# INS: Module "Install:" output color (cyan for ics)
+ifneq ($(BUILD_WITH_COLORS),false)
+    include $(TOP_DIR)vendor/neko/build/tools/colors.mk
+endif
+
 NEKO_TARGET_PACKAGE := $(PRODUCT_OUT)/$(NEKO_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
@@ -23,4 +32,19 @@ SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 neko: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(NEKO_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(NEKO_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(NEKO_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(NEKO_TARGET_PACKAGE)" >&2
+	@echo -e "Build complete"
+	@echo -e ${CL_RED}"========================================================================="${CL_RST}
+	@echo -e ""
+	@echo -e ${CL_CYN}"   ███╗   ██╗███████╗██╗  ██╗ ██████╗ ███████╗███████╗███████╗████████╗  "${CL_RST}
+	@echo -e ${CL_CYN}"   ████╗  ██║██╔════╝██║ ██╔╝██╔═══██╗██╔════╝██╔════╝██╔════╝╚══██╔══╝  "${CL_RST}
+	@echo -e ${CL_CYN}"   ██╔██╗ ██║█████╗  █████╔╝ ██║   ██║█████╗  █████╗  ███████╗   ██║     "${CL_RST}
+	@echo -e ${CL_CYN}"   ██║╚██╗██║██╔══╝  ██╔═██╗ ██║   ██║██╔══╝  ██╔══╝  ╚════██║   ██║     "${CL_RST}
+	@echo -e ${CL_CYN}"   ██║ ╚████║███████╗██║  ██╗╚██████╔╝██║     ███████╗███████║   ██║     "${CL_RST}
+	@echo -e ${CL_CYN}"   ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚══════╝╚══════╝   ╚═╝     "${CL_RST}
+	@echo -e ""
+	@echo -e ${CL_RED}"========================================================================="${CL_RST}
+	@echo -e "  NekoFest building complete!!!"
+	@echo -e "  Filename : $(NEKO_TARGET_PACKAGE)"
+	@echo -e "  Size     : `ls -lah $(NEKO_TARGET_PACKAGE) | cut -d ' ' -f 5`"
+	@echo -e "  SHA256   : `cat $(NEKO_TARGET_PACKAGE).sha256sum | cut -d ' ' -f 1`"
+	@echo -e ${CL_RED}"====================================================================="${CL_RST}
