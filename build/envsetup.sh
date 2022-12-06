5cbbf7457b57025c10d6586dc4ca033f23d7ecd8
Vayruz Rafli <frost@nusantararom.org>
2022-12-06 06:24:01 +0000

    envsetup: Add some function from PixelExperience
     * https://github.com/PixelExperience/vendor_aosp/blob/thirteen/build/envsetup.sh
    
    Signed-off-by: Vayruz Rafli <frost@nusantararom.org>
    Change-Id: Idb1bf4e386422889db4b29a609a56d25c2370f3f

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -4,6 +4,8 @@ Additional NekoFest functions:
 - clomerge:        Utility to merge CLO tags.
 - repopick:        Utility to fetch changes from Gerrit.
 - sort-blobs-list: Sort proprietary-files.txt sections with LC_ALL=C.
+- mka:             Builds using SCHED_BATCH on all processors.
+- cmka:            Cleans and builds using mka.
 EOF
 }
 
@@ -24,6 +26,68 @@ function repopick()
 function sort-blobs-list() {
     T=$(gettop)
     $T/tools/extract-utils/sort-blobs-list.py $@
+}
+
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        mka bacon
+    else
+        echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+    return $?
+}
+
+function breakfast()
+{
+    target=$1
+    local variant=$2
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        if [[ "$target" =~ -(user|userdebug|eng)$ ]]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the model name
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+
+            lunch neko_$target-$variant
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
+
+function mka() {
+    m "$@"
+}
+
+function cmka() {
+    if [ ! -z "$1" ]; then
+        for i in "$@"; do
+            case $i in
+                bacon|otapackage|systemimage)
+                    mka installclean
+                    mka $i
+                    ;;
+                *)
+                    mka clean-$i
+                    mka $i
+                    ;;
+            esac
+        done
+    else
+        mka clean
+        mka
+    fi
 }
 
 export SKIP_ABI_CHECKS="true"
