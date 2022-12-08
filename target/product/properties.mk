3167c7dda676ba1ac2d76ab3393d3b859f8375ac
Vayruz Rafli <frost@nusantararom.org>
2022-12-08 12:22:36 +0000

    vendor: Import Google-specific properties from hentaiOS
     * https://github.com/hentaiOS/platform_vendor_hentai/blob/TwistedScarlett/config/common.mk#L22
    
    Signed-off-by: Vayruz Rafli <frost@nusantararom.org>
    Change-Id: I8f1b87d03e0ea27d248813b607acd72f7fa32266

--- target/product/properties.mk
+++ target/product/properties.mk
@@ -47,3 +47,23 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # Enable Sense service for 64-bit only
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.face.sense_service=$(TARGET_SUPPORTS_64_BIT_APPS)
+
+# SetupWizard configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.theme=glif_v3_light
+
+# Google Play services configuration
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
